6dc317043918a723f615afeaf98264bf83fe3a00
ashin <ashin.jnv@gmail.com>
2021-07-06 09:45:19 +0530

    Update app.js

--- app.js
+++ app.js
@@ -28,15 +28,15 @@ app.use(function (req, res, next) {
 
 function verifyToken(req, res, next) {//token
         if (!req.headers.authorization) {
-            return res.status(401).send('Unauthorized request')
+            return res.status(200).send('Unauthorized request')
         }
         let token = req.headers.authorization.split(' ')[1]
         if (token === 'null') {
-            return res.status(401).send('Unauthorized request')
+            return res.status(200).send('Unauthorized request')
         }
         let payload = jwt.verify(token, 'secretKey')
         if (!payload) {
-            return res.status(401).send('Unauthorized request')
+            return res.status(200).send('Unauthorized request')
         }
         req.userId = payload.subject
         next()
